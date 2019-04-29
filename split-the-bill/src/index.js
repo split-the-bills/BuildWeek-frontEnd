70345f6393d8dadda7ce153bdd6d10ce1bc2487f
unknown <kcmp711@gmail.com>
2019-04-29 09:12:42 -0400

    imported Router in index.js

--- split-the-bill/src/index.js
+++ split-the-bill/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import { BrowserRouter as Router } from "react-router-dom";
 
 import reducer from './reducers';
 import App from './App';
